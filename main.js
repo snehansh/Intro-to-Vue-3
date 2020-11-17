b94127d01e9d1765cd4ac4e65cd42d7b80b9c9d4
Snehansh Akhaury <snehansh@protonmail.com>
2020-11-17 14:53:28 -0600

    L6-end

--- main.js
+++ main.js
@@ -9,7 +9,18 @@ const app = Vue.createApp({
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+            ],
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
+        },
+        removeFromCart() {
+            this.cart -= 1
         }
     }
 })
