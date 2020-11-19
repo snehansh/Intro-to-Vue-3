c5be68d905175f0e4603400e1bc4a8b5c41bda84
Snehansh Akhaury <snehansh@protonmail.com>
2020-11-19 14:05:06 -0600

    L10-end code challenge incomplete

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,12 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <button
+          class="button"
+          @click="removeFromCart">
+          Remove from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,10 +62,14 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          // this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       }
   },
   computed: {
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            // cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        // updateCart() {
+        // updateCart(id) {
+        //     // this.cart += 1
+        //     this.cart.push(id)
+        // }
+        addItem(id) {
+            // this.cart += 1
+            this.cart.push(id)
+        },
+        removeItem(id) {
+            // this.cart += 1
+            //incorrect
+            this.cart.splice(id)
+        }
+    }
 })
